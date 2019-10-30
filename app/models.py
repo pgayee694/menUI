79d7400a42f0390bbd8df4f843ca80063f533c51
pgayee694 <pgayee694@gmail.com>
2019-10-30 13:02:16 -0500

    added location table and cleaned up others

--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from app import db
 class User(db.Model):
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True)
-    password = db.Column(db.String(128))
-    location = db.Column(db.String(128))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    username = db.Column(db.String(64), index=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('location.id'), nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -14,22 +14,30 @@ class User(db.Model):
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(64), nullable=False)
 
     def __repr(self):
         return '<Restaurant {}>'.format(self.name)
 
-class User_Restaurants(db.Model):
-    __tablename__ = 'user_restaurants'
+class UserRestaurant(db.Model):
+    __tablename__ = 'user_restaurant'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'))
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurant.id'), nullable=False)
 
 class Friends(db.Model):
     __tablename__ = 'friends'
 
+    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    friend1_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    friend2_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Location(db.Model):
+    __tablename__ = 'location'
+
     id = db.Column(db.Integer, primary_key=True)
-    friend1_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    friend2_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    city = db.Column(db.String(64), nullable=False)
+    region = db.Column(db.String(64), nullable=False)
+    country = db.Column(db.String(64), nullable=False)
--- test/test_models.py
+++ test/test_models.py
@@ -20,15 +20,31 @@ class ModelsTest(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
+    def test_location(self):
+        l = models.Location(city='New York City', region='New York State', country='United States')
+        db.session.add(l)
+        db.session.commit()
+
+        locations = models.Location.query.all()
+        self.assertEqual(len(locations), 1)
+        self.assertEqual(locations[0].city, 'New York City')
+        self.assertEqual(locations[0].region, 'New York State')
+        self.assertEqual(locations[0].country, 'United States')
+
     def test_user(self):
-        u = models.User(username='test', password='asdf', location='nyc')
+        l = models.Location(city='New York City', region='New York State', country='United States')
+        db.session.add(l)
+        db.session.commit()
+
+        u = models.User(username='test', password='asdf', location_id=l.id)
         db.session.add(u)
         db.session.commit()
 
         users = models.User.query.all()
         self.assertEqual(len(users), 1)
         self.assertEqual(users[0].username, 'test')
-    
+        self.assertEqual(users[0].location_id, l.id)
+
     def test_restaurant(self):
         r = models.Restaurant(name='test')
         db.session.add(r)
@@ -38,21 +54,42 @@ class ModelsTest(unittest.TestCase):
         self.assertEqual(len(rs), 1)
         self.assertEqual(rs[0].name, 'test')
 
-    def test_user_restaurants(self):
-        u = models.User(username='test', password='asdf', location='nyc')
+    
+    def test_user_restaurant(self):
+        l = models.Location(city='New York City', region='New York State', country='United States')
+        db.session.add(l)
+        db.session.commit()
+
+        u = models.User(username='test', password='asdf', location_id=l.id)
+        db.session.add(u)
+        db.session.commit()
+
         r = models.Restaurant(name='test')
-        user_restaurant = models.User_Restaurants(user_id=u.id, restaurant_id=r.id)
+        db.session.add(r)
+        db.session.commit()
+
+        user_restaurant = models.UserRestaurant(user_id=u.id, restaurant_id=r.id)
         db.session.add(user_restaurant)
         db.session.commit()
 
-        user_restaurants = models.User_Restaurants.query.all()
+        user_restaurants = models.UserRestaurant.query.all()
         self.assertEqual(len(user_restaurants), 1)
         self.assertEqual(user_restaurants[0].user_id, u.id)
         self.assertEqual(user_restaurants[0].restaurant_id, r.id)
-    
+
     def test_friends(self):
-        u1 = models.User(username='test1', password='asdf1', location='nyc')
-        u2 = models.User(username='test2', password='asdf2', location='nyc')
+        l = models.Location(city='New York City', region='New York State', country='United States')
+        db.session.add(l)
+        db.session.commit()
+
+        u1 = models.User(username='test1', password='asdf1', location_id = l.id)
+        db.session.add(u1)
+        db.session.commit()
+
+        u2 = models.User(username='test2', password='asdf2', location_id = l.id)
+        db.session.add(u2)
+        db.session.commit()
+
         friendship = models.Friends(friend1_id=u1.id, friend2_id=u2.id)
         db.session.add(friendship)
         db.session.commit()
