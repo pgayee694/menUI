3089a63736f9b2e35e25bc371c7b69329b44d5f3
bbuffington1662 <bbuffington1662@gmail.com>
2019-11-17 17:46:15 -0600

    Menu Browsing Page  Front End

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,16 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+	
+@app.route('/menu-search', methods=['GET'])
+def menu_search():
+        return render_template('menu-search.html',
+		title='Menu Search')
+		
+@app.route('/menu-browse', methods=['GET'])
+def menu_browse():
+        return render_template('menu-browse.html',
+		title='Menu Browse')
