e812d67dd06c26e52cd3aa6ade340d73f28ba1c0
cheddartot <ethankschantz@gmail.com>
2019-11-17 19:45:45 -0600

    Currently Broken, adding database functionality to log in and sign up
    Currently trying to move sign in information to the database.

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 class User(db.Model):
     __tablename__ = 'user'
@@ -10,6 +12,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
-from app import app
-from flask import Flask, render_template, flash, redirect
+from app import app, models, db
+from flask import Flask, render_template, flash, redirect, request
 from app.forms import LoginForm, SignInForm
+from werkzeug.urls import url_parse
+from flask_login import login_user
 
 @app.route('/')
 def hello():
@@ -12,14 +14,31 @@ def login():
     if form_log_in.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form_log_in.username.data, form_log_in.remember_me.data))
+        user = models.User.query.filter_by(Username=form_log_in.username.data).first()
+        if user is None or not user.check_password(form_log_in.password.data):
+            flash('invalid username or password')
+            return redirect('/login/')
+        login_user(user, remember=form_log_in.remember_me.data)
         return redirect('/')
     return render_template('login.html', title='Log In', form=form_log_in)
 
 @app.route('/signup/', methods=['GET', 'POST'])
 def signup():
     form_sign_up = SignInForm()
-    print(form_sign_up.username.data)
     if form_sign_up.validate_on_submit() and form_sign_up.password.data == form_sign_up.password2.data:
+        #before creating a location query database and see if it exists already
+        #if it doesn't, make a location object and add to DB before building user
+
+        #CURRENTLY THIS IS DOING NO VALIDATION AND ALWAYS ADDING USERS TO THE DATABASE
+        loc = models.Location(city=form_sign_up.city.data, region=form_sign_up.region.data, country='placeholder')
+        db.session.add(loc)
+        #potentially uncomment this if you need to commit this before user
+        #db.session.commit()
         flash('Sign up requested for user{}'.format(form_sign_up.username.data))
+        user = models.User(username=form_sign_up.username.data, location_id=loc.id)
+        user.set_password(form_sign_up.password.data)
+        db.session.add(user)
+        db.session.commit()
+
         return redirect('/')
     return render_template('signup.html', title='Sign Up', form=form_sign_up)
