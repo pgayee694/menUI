afde02f5cc24e62fba2e253250b919426169b474
cheddartot <ethankschantz@gmail.com>
2019-11-17 20:48:43 -0600

    Added sign-up and log-in functionality
    Currently allows users to sign up and log in to their accounts.

--- app/models.py
+++ app/models.py
@@ -1,8 +1,12 @@
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
@@ -16,8 +20,8 @@ class User(db.Model):
     def set_password(self, password):
         self.password = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
+    def check_password(self, password_passed):
+        return check_password_hash(self.password, password_passed)
 
 class Restaurant(db.Model):
     __tablename__ = 'restaurant'
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
 from app import app, models, db
 from flask import Flask, render_template, flash, redirect, request
 from app.forms import LoginForm, SignInForm
-from werkzeug.urls import url_parse
-from flask_login import login_user
+from .models import db, User, Location
+from flask_login import login_user, current_user
 
 @app.route('/')
 def hello():
@@ -11,10 +11,12 @@ def hello():
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     form_log_in = LoginForm()
+    if current_user.is_authenticated:
+        return redirect('/')
     if form_log_in.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form_log_in.username.data, form_log_in.remember_me.data))
-        user = models.User.query.filter_by(Username=form_log_in.username.data).first()
+        user = models.User.query.filter_by(username=form_log_in.username.data).first()
         if user is None or not user.check_password(form_log_in.password.data):
             flash('invalid username or password')
             return redirect('/login/')
@@ -28,17 +30,17 @@ def signup():
     if form_sign_up.validate_on_submit() and form_sign_up.password.data == form_sign_up.password2.data:
         #before creating a location query database and see if it exists already
         #if it doesn't, make a location object and add to DB before building user
-
+        db.create_all()
         #CURRENTLY THIS IS DOING NO VALIDATION AND ALWAYS ADDING USERS TO THE DATABASE
         loc = models.Location(city=form_sign_up.city.data, region=form_sign_up.region.data, country='placeholder')
         db.session.add(loc)
         #potentially uncomment this if you need to commit this before user
-        #db.session.commit()
+        db.session.commit()
         flash('Sign up requested for user{}'.format(form_sign_up.username.data))
         user = models.User(username=form_sign_up.username.data, location_id=loc.id)
         user.set_password(form_sign_up.password.data)
         db.session.add(user)
         db.session.commit()
 
-        return redirect('/')
+        return redirect('/login/')
     return render_template('signup.html', title='Sign Up', form=form_sign_up)
