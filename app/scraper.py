fd0ecb2704df360cea790d666633683319db78c6
pgayee694 <pgayee694@gmail.com>
2019-12-08 12:13:06 -0600

    scraping zomato pictures

--- app/scraper.py
+++ app/scraper.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+import re
+import json
 from app import view_models
+
 
 def parse_zomato(url):
 
     headers = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(url=url, headers=headers)
+    response = requests.get(url='https://www.zomato.com/omaha/salween-thai-omaha/menu', headers=headers)
 
     menu_items = []
 
@@ -13,8 +16,13 @@ def parse_zomato(url):
         soup = BeautifulSoup(response.text, 'html.parser')
 
         if soup.find('div', id='menu-image'):
-            #time to selenium
+            #menu is a picture(s)
+            json_string = soup.find(text=re.compile('menuPages')).strip().split('\n')[1].strip().split()[2][:-1]
+            json_obj = json.loads(json_string)
+            for i in range(len(json_obj)):
+                menu_items.append(MenuItem('Page {}'.format(i), None, json_obj[i]['url'].strip()))
         else:
+            #menu is just a page
             items = soup.find_all('div', class_='tmi-text-group')
 
             for item in items:
