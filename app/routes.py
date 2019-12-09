621345fabab32c3ade5e860d9861ba3a92a04439
bbuffington1662 <bbuffington1662@gmail.com>
2019-12-08 18:37:53 -0600

    added add ability

--- app/routes.py
+++ app/routes.py
@@ -90,3 +90,11 @@ def signup():
 def logout():
     logout_user()
     return redirect('/')
+	
+@app.route('/menu-add', methods=['GET', 'POST'])
+def menu_add():
+	
+	utils.add_user_restaurant(current_user.id, request.form.get('add'))
+	flash("Restaurant " + request.form.get('add') + " was added!!!")
+	
+	return render_template('home.html', title='Home')
